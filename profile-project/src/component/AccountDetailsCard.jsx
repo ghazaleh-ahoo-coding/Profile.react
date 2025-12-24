9ad4546111ea3ac035cf6044faa3bbe493445cfe
Ghazaleh Azimi <ghazalehazimi23@gmail.com>
2025-12-24 17:38:05 +0430

    Add AccountDetailsCard and StatCard components
    Introduced AccountDetailsCard component to display user account details with role, status, and verification. Added a placeholder StatCard component for future use.

---
+++ profile-project/src/component/AccountDetailsCard.jsx
@@ -0,0 +1,12 @@
+export default function AccountDetailsCard(details){
+    return(
+        <div classname="card UnfoCard">
+            <h2 className="cardtitle"> Account Details</h2>
+            <div className="rows">
+                <Row label="Role" value={details.Role}></Row>
+                <Row label="Status" value={<span className='pill'>Active</span>}></Row>
+                <Row label="Verification" value={<span className='verified'>✔️Verified</span>}></Row>
+            </div>
+        </div>
+    )
+}
