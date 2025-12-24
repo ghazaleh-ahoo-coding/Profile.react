e5aededbfef611ee4dac278657a7664b2e9a7647
Ghazaleh Azimi <ghazalehazimi23@gmail.com>
2025-12-24 19:41:17 +0430

    Update personalInfoCard.jsx

--- profile-project/src/component/personalInfoCard.jsx
+++ profile-project/src/component/personalInfoCard.jsx
@@ -2,11 +2,12 @@ import Row from "./Row";
 
 export default function PersonalInfoCard({ info }) {
   return (
-    <div className="Card infocard">
+    <div className="card infoCard">
       <h2 className="cardTitle">Personal Information</h2>
+
       <div className="rows">
         {Object.keys(info).map((label) => (
-          <Row label={label} value={info[label]} />
+            <Row key={label} label={label} value={info[label]}></Row>
         ))}
       </div>
     </div>
