095a17e4fb0ea45eeab8b0d0f445f4d328db6bcb
Ghazaleh Azimi <ghazalehazimi23@gmail.com>
2025-12-24 17:59:15 +0430

    Update StatCard.jsx

--- profile-project/src/component/StatCard.jsx
+++ profile-project/src/component/StatCard.jsx
@@ -1,14 +1,12 @@
-export default function StatCard ((title, bigValue, footerText, icon)){
-    return(
-        <div className="card statCard">
-            <div className="statTop">
-                <p className="satatTitle">
-                    {title}
-                </p>
-                {icon ? <span className="statIcon">{icon}</span>: null}
-            </div>
-              {bigValue ? <span className="statIcon">{icon}</span>: null}
-              <p className="statFooter">{footerText}</p>
-        </div>
-    )
+export default function StatCard({ title, bigValue, footerText, icon }) {
+  return (
+    <div className="card statCard">
+      <div className="statTop">
+        <p className="statTitle">{title}</p>
+        {icon ? <span className="statIcon">{icon}</span> : null}
+      </div>
+      {bigValue ? <span className="statValue">{bigValue}</span> : null}
+      <p className="statFooter">{footerText}</p>
+    </div>
+  );
 }
